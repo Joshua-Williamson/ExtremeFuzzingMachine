eb7a9c4c9b7ff36b523c9212c436d6388f3cfee7
Joshua Williamson <1joshua.williamson@gmail.com>
2022-03-07 13:35:18 +0000

    Converted accuracy to Torch and made a testing file for ELM

--- NN_test/nntest.py
+++ NN_test/nntest.py
@@ -45,16 +45,16 @@ def process_data():
     global MAX_FILE_SIZE
     global SPLIT_RATIO
     global seed_list
-    global new_seeds
+    global test_seed_list
+    global train_seed_list
 
     # shuffle training samples
     seed_list = glob.glob('./seeds/*')
     seed_list.sort()
-    SPLIT_RATIO = len(seed_list)
-    rand_index = np.arange(SPLIT_RATIO)
+    SPLIT_RATIO = int(len(seed_list)*TTR)
     np.random.shuffle(seed_list)
-    new_seeds = glob.glob('./seeds/id_*')
-
+    test_seed_list=seed_list[:SPLIT_RATIO]
+    train_seed_list=seed_list[SPLIT_RATIO:]
     call = subprocess.check_output
 
     # get MAX_FILE_SIZE
@@ -63,7 +63,8 @@ def process_data():
     MAX_FILE_SIZE = os.path.getsize(cwd + '/seeds/' + max_file_name)
 
     # create directories to save label, spliced seeds, variant length seeds, crashes and mutated seeds.
-    os.path.isdir("./bitmaps/") or os.makedirs("./bitmaps")
+    os.path.isdir("./train_bitmaps/") or os.makedirs("./bitmaps")
+    os.path.isdir("./test_bitmaps/") or os.makedirs("./bitmaps")
     os.path.isdir("./splice_seeds/") or os.makedirs("./splice_seeds")
     os.path.isdir("./vari_seeds/") or os.makedirs("./vari_seeds")
     os.path.isdir("./crashes/") or os.makedirs("./crashes")
@@ -72,7 +73,7 @@ def process_data():
     raw_bitmap = {} #Is a dictionary for each seed file key containing the sequential ID's of each branch it covered
     tmp_cnt = [] #Hold's ID's cumlatively for each seed input
     out = ''
-    for f in seed_list:
+    for f in train_seed_list:
         tmp_list = [] #Keeps list of ID's for each seed file inside loop
         try:
             infile=open(f,'r')
@@ -100,8 +101,8 @@ def process_data():
     # save bitmaps to individual numpy label
     # creates array of N_seed x Total edges found and for each seed assigns a one for an edge it touches and 0 if not
     label = [int(f[0]) for f in counter]
-    bitmap = np.zeros((len(seed_list), len(label)))
-    for idx, i in enumerate(seed_list):
+    bitmap = np.zeros((len(train_seed_list), len(label)))
+    for idx, i in enumerate(train_seed_list):
         tmp = raw_bitmap[i]
         for j in tmp:
             if int(j) in label:
@@ -114,24 +115,81 @@ def process_data():
 
     # save training data
     MAX_BITMAP_SIZE = fit_bitmap.shape[1]
-    for idx, i in enumerate(seed_list):
-        file_name = "./bitmaps/" + i.split('/')[-1]
+    for idx, i in enumerate(train_seed_list):
+        file_name = "./train_bitmaps/" + i.split('/')[-1]
+        np.save(file_name, fit_bitmap[idx])
+
+    # obtain raw bitmaps
+    raw_bitmap = {} #Is a dictionary for each seed file key containing the sequential ID's of each branch it covered
+    tmp_cnt = [] #Hold's ID's cumlatively for each seed input
+    out = ''
+    for f in test_seed_list:
+        tmp_list = [] #Keeps list of ID's for each seed file inside loop
+        try:
+            infile=open(f,'r')
+            # append "-o tmp_file" to strip's arguments to avoid tampering tested binary.
+            mem_lim= '512' if not args.enable_asan else 'none'
+            if argvv[0] == './strip':
+                raise NotImplementedError
+                out = call(['./afl-showmap', '-q', '-e', '-o', '/dev/stdout', '-m', '512', '-t', '500'] + argvv + [f] + ['-o', 'tmp_file'])
+            else:
+                out = call(['./afl-showmap','-q', '-e', '-o', '/dev/stdout', '-m', mem_lim, '-t', '500'] + args.target ,stdin=infile)
+            infile.close()
+        except subprocess.CalledProcessError as e:
+            print('Weird afl-showmap bug again') #JW DBG
+            raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+
+        #Takes the first arg of each tuple generated 
+        #I.e Collecting A -> B -> C -> D -> E (tuples: AB, BC, CD, DE) = [ A , B, C, D, E ]
+        for line in out.splitlines():
+            edge = line.split(b':')[0]
+            tmp_cnt.append(edge)
+            tmp_list.append(edge)
+        raw_bitmap[f] = tmp_list
+    counter = Counter(tmp_cnt).most_common() #Counts the occurances of each edge (ID) [('ID',No.),...] ordered in decending order 
+
+    # save bitmaps to individual numpy label
+    # creates array of N_seed x Total edges found and for each seed assigns a one for an edge it touches and 0 if not
+    label = [int(f[0]) for f in counter]
+    bitmap = np.zeros((len(test_seed_list), len(label)))
+    for idx, i in enumerate(test_seed_list):
+        tmp = raw_bitmap[i]
+        for j in tmp:
+            if int(j) in label:
+                bitmap[idx][label.index((int(j)))] = 1
+
+    # label dimension reduction
+    # Kinda weird indepnedent of edge value reduces the bitmap to the different ways each seed can cross each edge
+    fit_bitmap = np.unique(bitmap, axis=1)
+    print("data dimension" + str(fit_bitmap.shape))
+
+    # save training data
+    MAX_BITMAP_SIZE = fit_bitmap.shape[1]
+    for idx, i in enumerate(test_seed_list):
+        file_name = "./test_bitmaps/" + i.split('/')[-1]
         np.save(file_name, fit_bitmap[idx])
 
 
 # training data generator
-def generate_training_data(lb, ub):
+def generate_training_data(tt,lb, ub):
+    if tt=='test':
+        list=test_seed_list
+        stub="./test_bitmaps/"
+    elif tt=='train': 
+        list=train_seed_list
+        stub="./train_bitmaps/"
+        
     seed = np.zeros((ub - lb, MAX_FILE_SIZE))
     bitmap = np.zeros((ub - lb, MAX_BITMAP_SIZE))
     for i in range(lb, ub):
-        tmp = open(seed_list[i], 'rb').read()
+        tmp = open(list[i], 'rb').read()
         ln = len(tmp)
         if ln < MAX_FILE_SIZE:
             tmp = tmp + (MAX_FILE_SIZE - ln) * b'\x00'
         seed[i - lb] = [j for j in bytearray(tmp)]
 
     for i in range(lb, ub):
-        file_name = "./bitmaps/" + seed_list[i].split('/')[-1] + ".npy"
+        file_name = stub + list[i].split('/')[-1] + ".npy"
         bitmap[i - lb] = np.load(file_name)
     return seed, bitmap
 
@@ -144,18 +202,17 @@ def step_decay(epoch):
     lrate = initial_lrate * math.pow(drop, math.floor((1 + epoch) / epochs_drop))
     return lrate
 
-def train_generate(batch_size):
-    global seed_list
-    np.random.shuffle(seed_list)
+def train_generate(tt,batch_size):
+
     # load a batch of training data
     for i in range(0, SPLIT_RATIO, batch_size):
         # load full batch if batchsize is greater than the seeds availible
         if (i + batch_size) > SPLIT_RATIO:
-            x, y = generate_training_data(i, SPLIT_RATIO)
+            x, y = generate_training_data(i, SPLIT_RATIO,tt)
             x = x.astype('float32') / 255
         # load remaining data for last batch
         else:
-            x, y = generate_training_data(i, i + batch_size)
+            x, y = generate_training_data(i, i + batch_size,tt)
             x = x.astype('float32') / 255
         yield (torch.Tensor(x), torch.Tensor(y))
 
@@ -215,119 +272,6 @@ def splice_seed(fl1, fl2, idxx):
         print(f_diff, l_diff)
         randd = random.choice(seed_list)
 
-
-# compute gradient for given input
-# taking gradient of randomly selected bitmap output at randomly selected input
-def gen_adv2(f, fl, model, idxx, splice):
-    adv_list = []
-    ll = 2
-    while fl[0] == fl[1]:
-        fl[1] = random.choice(seed_list)
-
-    for index in range(ll):
-        x = vectorize_file(fl[index])
-        out = model.forward_to_sig(x)[:,f]
-        grads_value = torch.autograd.grad(out,x)[0].numpy()
-        idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
-        val = np.sign(grads_value[0][idx])
-        adv_list.append((idx, val, fl[index]))
-
-    # do not generate spliced seed for the first round
-    if splice == 1 and round_cnt != 0:
-        if round_cnt % 2 == 0:
-            splice_seed(fl[0], fl[1], idxx)
-            x = vectorize_file('./splice_seeds/tmp_' + str(idxx))
-            out = model.forward_to_sig(x)[:,f]
-            grads_value = torch.autograd.grad(out,x).numpy()
-            idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
-            val = np.sign(grads_value[0][idx])
-            adv_list.append((idx, val, './splice_seeds/tmp_' + str(idxx)))
-        else:
-            splice_seed(fl[0], fl[1], idxx + 500)
-            x = vectorize_file('./splice_seeds/tmp_' + str(idxx + 500))
-            out = model.forward_to_sig(x)[:,f]
-            grads_value = torch.autograd.grad(out,x).numpy()
-            idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
-            val = np.sign(grads_value[0][idx])
-            adv_list.append((idx, val, './splice_seeds/tmp_' + str(idxx + 500)))
-
-    return adv_list
-
-
-# compute gradient for given input without sign
-def gen_adv3(f, fl, model, idxx, splice):
-    adv_list = []
-    ll = 2
-    while fl[0] == fl[1]:
-        fl[1] = random.choice(seed_list)
-
-    for index in range(ll):
-        x = vectorize_file(fl[index])
-        out = model.forward_to_sig(x)[:,f]
-        grads_value = torch.autograd.grad(out,x).numpy()
-        idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
-        #val = np.sign(grads_value[0][idx])
-        val = np.random.choice([1, -1], MAX_FILE_SIZE, replace=True)
-        adv_list.append((idx, val, fl[index]))
-
-    # do not generate spliced seed for the first round
-    if splice == 1 and round_cnt != 0:
-        splice_seed(fl[0], fl[1], idxx)
-        x = vectorize_file('./splice_seeds/tmp_' + str(idxx))
-        out = model.forward_to_sig(x)[:,f]
-        grads_value = torch.autograd.grad(out,x).numpy()
-        idx = np.flip(np.argsort(np.absolute(grads_value), axis=1)[:, -MAX_FILE_SIZE:].reshape((MAX_FILE_SIZE,)), 0)
-        # val = np.sign(grads_value[0][idx])
-        val = np.random.choice([1, -1], MAX_FILE_SIZE, replace=True)
-        adv_list.append((idx, val, './splice_seeds/tmp_' + str(idxx)))
-
-    return adv_list
-
-
-# grenerate gradient information to guide furture muatation
-def gen_mutate2(model, edge_num, sign):
-    
-    #model=Keras model, Edge_num=of paths to smaple as 'interesting', sign=True if train false if not
-    
-    tmp_list = []
-    # select seeds
-    print("#######debug" + str(round_cnt))
-    if round_cnt == 0:
-        new_seed_list = seed_list
-    else:
-        new_seed_list = new_seeds
-
-    if len(new_seed_list) < edge_num: #2 X 500 random samples of seed list
-        rand_seed1 = [new_seed_list[i] for i in np.random.choice(len(new_seed_list), edge_num, replace=True)]
-    else:
-        rand_seed1 = [new_seed_list[i] for i in np.random.choice(len(new_seed_list), edge_num, replace=False)]
-    if len(new_seed_list) < edge_num:
-        rand_seed2 = [seed_list[i] for i in np.random.choice(len(seed_list), edge_num, replace=True)]
-    else:
-        rand_seed2 = [seed_list[i] for i in np.random.choice(len(seed_list), edge_num, replace=False)]
-
-    # function pointer for gradient computation
-    fn = gen_adv2 if sign else gen_adv3
-
-    # select output neurons to compute gradient
-    interested_indice = np.random.choice(MAX_BITMAP_SIZE, edge_num)
-
-    with open('gradient_info_p', 'w') as f:
-        for idxx in range(len(interested_indice[:])):
-            print("number of feature " + str(idxx))
-            index = int(interested_indice[idxx])
-            fl = [rand_seed1[idxx], rand_seed2[idxx]]
-            adv_list = fn(index, fl, model, idxx, 1)
-            tmp_list.append(adv_list)
-            #Basically takes random inputs from the seed files and considers their gradient on a randomly selected
-            #bitmap and returns the gradients of each input byte w.r.t output 
-            for ele in adv_list:
-                ele0 = [str(el) for el in ele[0]]
-                ele1 = [str(int(el)) for el in ele[1]]
-                ele2 = ele[2]
-                f.write(",".join(ele0) + '|' + ",".join(ele1) + '|' + ele2 + "\n")
-
-
 def build_model():
     #Fixed batch size and epoch?
     batch_size = 32
@@ -354,7 +298,7 @@ def train(model,optimizer):
     batch_size=16
     init = time.time()
     model.train()
-    for batch_idx, (data, target) in enumerate(train_generate(batch_size)):
+    for batch_idx, (data, target) in enumerate(train_generate(batch_size,tt='train')):
         if args.enable_cuda:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data,requires_grad=True, volatile=False), \
@@ -363,9 +307,27 @@ def train(model,optimizer):
         optimizer.train(inputs=hiddenOut, targets=target)
         output = model.forward(data)
         pred=output.data.max(1)[1]
+        acc=accur_1(target,pred)
 
     ending = time.time()
-    print('training time: {:.2f}sec'.format(ending - init))
+    print('Training time: {:.2f}sec/ Training Accuracy: {:.2f}'.format(ending - init,acc))
+
+def test(model):
+    batch_size=16
+    init = time.time()
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_generate(batch_size,tt='test')):
+        if args.enable_cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data,requires_grad=True, volatile=False), \
+                       Variable(target.type(torch.float32),requires_grad=True, volatile=False)
+        output = model.forward(data)
+        pred=output.data.max(1)[1]
+        acc=accur_1(target,pred)
+
+    ending = time.time()
+    print('Testing time: {:.2f}sec/ Testing Accuracy: {:.2f}'.format(ending - init, acc))
+
 
 
 def gen_grad(data):
@@ -374,34 +336,8 @@ def gen_grad(data):
     process_data()
     model,optimiser = build_model()
     train(model,optimiser)
-    gen_mutate2(model, 500, data[:5] == b"train") #500 -> 100 in paper
-    round_cnt = round_cnt + 1
+    test(model,optimiser)
     print(time.time() - t0)
-
-
-def setup_server():
-    #Initalise server config
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #Such that the OS releases the port quicker for rapid rerunning
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
-    #Attatch to ip and port
-    sock.bind((HOST, PORT))
-    #Waits for neuzz execution
-    sock.listen(1)
-    print("Waiting for neuzz engine")
-    conn, addr = sock.accept()
-    print('Connected by neuzz engine ' + str(addr))
-    gen_grad(b"train")
-    conn.sendall(b"start")
-    while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        else:
-            gen_grad(data)
-            conn.sendall(b"start")
-    conn.close()
 
 
 if __name__ == '__main__':
@@ -425,4 +361,4 @@ if __name__ == '__main__':
     global args
     args = parser.parse_args()
     #Start program and spin up server
-    setup_server()
+    gen_grad()
