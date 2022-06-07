a837731794b17c94874425e309e6c9047d279a1a
Joshua <1joshua.williamson@gmail.com>
2022-06-07 13:55:16 +0000

    WIP

--- nn-new.py
+++ nn-new.py
@@ -18,6 +18,7 @@ from torch.utils.data import Dataset
 import sysv_ipc as ipc
 from flow import FlowBuilder
 from TorchELM import pseudoInverse 
+import utils
 
 #Setting up ip and port for internal server
 HOST = '127.0.0.1'
@@ -27,15 +28,96 @@ class Edge_Data(Dataset):
 
     """ 
     Inherits from torch Dastasets and allows the edge information to be read into the 
-    training Torch models easily.
+    training Torch models easily. Handles data curation and processing.
     """
 
     def __init__(self, EFM_obj):
+        self.EFM=EFM_obj
         self.seeds_path=glob.glob('./seeds/*')
         self.nocov_path=glob.glob('./nocov/*')
-        
 
-class EFM():
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def process_bitmaps
+
+class Extreme_Fuzzing_Machine():
     
+    #Setters and properties to update the stats
+    #Not that important, ignore till the :)
+    @property
+    def accuracy(self):
+        return self._accuracy
+
+    @utils.shm_stats
+    @accuracy.setter
+    def accuracy(self, value):
+        self._accuracy=value
+        return value
+
+    @property
+    def bitmap_size(self):
+        return self._bitmap_size
+
+    @utils.shm_stats
+    @bitmap_size.setter
+    def bitmap_size(self, value):
+        self._bitmap_size=value
+        return value
+
+    @property
+    def corpus_size(self):
+        return self._corpus_size
+
+    @utils.shm_stats
+    @corpus_size.setter
+    def corpus_size(self, value):
+        self._corpus_size=value
+        return value
+
+    @property
+    def last_mapping(self):
+        return self._last_mapping
+
+    @utils.shm_stats
+    @last_mapping.setter
+    def last_mapping(self, value):
+        self._last_mapping=value
+        return value
+
+    @property
+    def last_training(self):
+        return self._last_training
+
+    @utils.shm_stats
+    @last_training.setter
+    def last_training(self, value):
+        self._last_training=value
+        return value
+
+    @property
+    def num_grads(self):
+        return self._num_grads
+
+    @utils.shm_stats
+    @num_grads.setter
+    def num_grads(self, value):
+        self._num_grads=value
+        return value
+    
+    #:)
+
     def __init__(self):
-        raise NotImplemented
+        self._accuracy=None
+        self._bitmap_size=None
+        self._corpus_size=None
+        self._last_mapping=None
+        self._last_training=None
+        self._num_grads=None
+
+if __name__ == "__main__":
+    EFM=Extreme_Fuzzing_Machine()
+    utils.shm_stats.SHM_obj=EFM
--- utils/utils.py
+++ utils/utils.py
@@ -6,6 +6,9 @@ import shlex
 import subprocess
 import logging
 
+import sysv_ipc as ipc
+import functools
+
 
 def init_logger(file_name, verbose=1, name=None):
     level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
@@ -67,3 +70,60 @@ def action(line: str):
     if not match_obj:
         return 'None'
     return match_obj.group(1)
+
+#Shared memory management 
+class SHM(object):
+    def __init__(self):
+        #Make it so theses keys are the variable/function names 
+        self.stats={"status":"-",
+                    "accuracy":"-",
+                    "bitmap_size":"-",
+                    "corpus_size":"-",
+                    "nocov_size":"-",
+                    "last_mapping":"-",
+                    "last_reducing":"-",
+                    "last_training":"-",
+                    "num grads":"-"
+        }
+
+        #Get this to update the status based on the function name
+        self.status_table={"":"Mapping",
+                           "":"Training",
+                           "":"Generating Grads",
+                           "":"Sleeping",
+                           "":"Remapping",
+                           "":"T-mining",
+                           "":"Culling Nocov"
+        }
+                           
+
+        #Attatch to shared memory from the c module
+        self.shm = ipc.SharedMemory(ipc.ftok("/tmp", 6667,silence_warning = True), 0, 0) 
+        self.shm.attach(0,0)
+
+    def __call__(self, func, *args, **kwargs):
+        
+        if func.__name__ in self.status_table:
+            self.stats["status"] = self.status_table[func.__name__]
+
+        else: 
+            val = func(*args, **kwargs)
+            self.stats[func.__name__] = val
+
+    def update_shm_buff(self):
+        
+        msg=bytearray()
+        for ele in self.stats.values():
+            null_pad=40 - (len(ele)+1) 
+            msg.extend(ele.encode('utf-8'))
+            for i in range(null_pad) :
+                msg.append(0)
+
+        self.shm.write(bytes(msg))
+
+
+def shm_stats(func):
+    @functools.wraps(func)
+    def wrapper_shm(*args, **kwargs):
+        shm_stats.SHM_obj(func, *args, **kwargs)
+    return wrapper_shm
